7e555323e6f1420270837cbe7663cd1c1d00bcac
magicmatteo <57829614+magicmatteo@users.noreply.github.com>
2019-11-24 10:28:16 +1100

    Added requirements.txt, Added non csv mode to program, updated README
    I have added a way of using the program without specifying a CSV file.
    It uses a menu that allows you to add apps to the list one by one and then select done once you've finished adding them.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # bundleIDfinder
 
-I was having trouble.. sort of.. manual labour trouble.. gettin the bundle ID's for ipad apps for use in my MDM (MS intune). To exclude certain apps from view or to reorganise them as per your design, you need to specify the apps Bundle ID. This became a 3 step process for me. I first needed a way of searching the app store.. which isnt native on a windows PC using a web browser.. I found a site (that is no longer maintained I believe) called theappstore.org.
+I was having trouble.. sort of.. manual labour trouble.. getting the bundle ID's for ipad apps for use in my MDM (MS intune). To exclude certain apps from view or to reorganise them as per your design, you need to specify the apps Bundle ID. This became a 3 step process for me. I first needed a way of searching the app store.. which isnt native on a windows PC using a web browser.. I found a site (that is no longer maintained I believe) called theappstore.org.
 
 This site allows you to search the app store basically. Upon finding the correct app on this, you need to click the link to get the app ID ie 998767232.. This is a long numeric string. You then need to insert this into an apple URL to lookup the app, which return a txt file download with some JSON data that contains your bundle ID at the end.
 
@@ -17,17 +17,20 @@ I will post the --help argument output below:
 		Welcome to the Apple App BundleID finder.
 		-------------------------------------
 
-		This takes input from a CSV containing the names of apps you want a Bundle ID for.
-		The CSV does not need a header. Place all apps in the first column.
-
 		** IMPORTANT **
 		You must use the most accurate title you can. Ideally the title as displayed in
 		the app store.
 
-		To use program, run it with an argument of your chosen CSV file.
+		To use program, either run it with no arugument or with the argument --csv to specifiy
+		a csv file with a list of apps.
 
-		Example: ./bundleID.py my_app_list.csv
+		Example: ./bundleID.py
+		When running without arguments, you will be prompted to add apps to the list by selecting the 'A' option in the menu. When you have finished adding apps, choose the 'D' option to return your reults.
 
-		The program will output "results.csv" with all of the matched data.
+		-- csv:
+		Example: ./bundleID.py --csv my_app_list.csv
+		This takes input from a CSV containing the names of apps you want a Bundle ID for.
+		The CSV does not need a header. Place all apps in the first column.
+		When using the csv option, the program will output "results.csv" with all of the matched data.
 
 		Please contact magicmatteo on GitHub for issues.
--- __main__.py
+++ __main__.py
@@ -1,116 +1,171 @@
-
 from difflib import SequenceMatcher
 from bs4 import BeautifulSoup
 from urllib import request
 import requests, csv, sys, os
 
-if len(sys.argv) < 2:
-	print("\nYou Must enter a csv filename! For example...\nbundleid.py apps.csv\n For help run --help")
-	sys.exit()
+help_text = '''
+Welcome to the Apple App BundleID finder.
+-------------------------------------------
 
-if sys.argv[1].lower() == '--help':
-	print(
-		'''
-		Welcome to the Apple App BundleID finder.
-		-------------------------------------
+** IMPORTANT **
+You must use the most accurate title you can. Ideally the title as displayed in
+the app store.
 
-		This takes input from a CSV containing the names of apps you want a Bundle ID for.
-		The CSV does not need a header. Place all apps in the first column.
+To use program, either run it with no arugument or with the argument --csv to specifiy
+a csv file with a list of apps.
 
-		** IMPORTANT **
-		You must use the most accurate title you can. Ideally the title as displayed in
-		the app store.
+Example: ./bundleID.py
+When running without arguments, you will be prompted to add apps to the list by selecting the 'A' option in the menu. When you have finished adding apps, choose the 'D' option to return your reults.
 
-		To use program, run it with an argument of your chosen CSV file.
+-- csv:
+Example: ./bundleID.py --csv my_app_list.csv
+This takes input from a CSV containing the names of apps you want a Bundle ID for.
+The CSV does not need a header. Place all apps in the first column.
+When using the csv option, the program will output "results.csv" with all of the matched data.
 
-		Example: ./bundleID.py my_app_list.csv
+Please contact magicmatteo on GitHub for issues.
+'''
 
-		The program will output "results.csv" with all of the matched data.
+def program_mode():
+	global mode
+	
+	if len(sys.argv) < 2:
+		mode = 'single'
 
-		Please contact magicmatteo on GitHub for issues.
+	elif sys.argv[1].lower() == '--csv':
+		mode = 'csv'
 
-		''')
-	sys.exit()
+	elif sys.argv[1].lower() == '--help':
+		print(help_text)
+		sys.exit()
 
-if os.path.exists(sys.argv[1]):
-	reader = csv.reader(open(sys.argv[1]))
-else:
-	print("File not found. Please enter a valid CSV filename.")
+def single_menu():
+	count = 0
+	while True:
+		if count == 0:
+			print('Welcome to the bundleIDfinder.. run bundleIDfinder --help for help.\nPlease choose from the following options:')
+			count =+ 1
+		selection = input('A: Add app to list\nD: Done adding apps\nQ: Quit\n')
+		if selection.lower() == 'a':
+			a = input('Enter name of app: ')
+			apps.append(a)
+			print(f'{a} added')
 
-#Check for http proxy settings
+		elif selection.lower() == 'd':
+			break
+		elif selection.lower() == 'q':
+			sys.exit()
+		else: print('Please enter a valid response')
+
+program_mode()
+
+# Check for http proxy settings
 systemProxy = request.getproxies()
 
-# Parse through CSV adding each entry to apps LIST.
+# Declare apps list outside of loops
 apps = []
-for row in reader:
-	apps.append(row[0])
+
+# if CSV mode, Parse through CSV adding each entry to apps LIST.
+if mode == 'csv':
+	if os.path.exists(sys.argv[2]):
+		reader = csv.reader(open(sys.argv[2]))
+		try:
+			for row in reader:
+				apps.append(row[0])
+		except IndexError:
+			print('Please verify CSV file. Consult --help for help.')
+			sys.exit()
+
+	else:
+		print("File not found. Please enter a valid CSV filename.")
+		sys.exit()
+
+
+if mode == 'single':
+	single_menu()
+
 
 # This function grabs the numeric ID out of the app store URL.
-def find_ID(url):
+def find_id(url):
 	return url.partition('id')[2].partition('?')[0]
+
 
 # Declare matches outside of loops to prevent overwrite
 matches = []
 failures = []
 
 for app in apps:
-	appToFind = app
-	appForURL = appToFind.translate({ord(' '): ord('-')}).lower() # Translate sapces to hyphons for URL
+    appToFind = app
+    appForURL = appToFind.translate({ord(' '): ord('-')}).lower()  # Translate sapces to hyphons for URL
 
-	count = 0
+    count = 0
 
-	# Skip apps with symbols and explain.
-	if set('[~!@#$%^&*()_+{}":;\']+$').intersection(app):
-		print('We found symbols in: %s - Please remove them' % app)
-		continue
+    # Skip apps with symbols and explain.
+    if set('[~!@#$%^&*()_+{}":;\']+$').intersection(app):
+        print('We found symbols in: %s - Please remove them' % app)
+        continue
 
-	h = {'rawUa': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.1 Safari/605.1.15',
-	  'referer': 'https://google.com.au'}
+    h = {
+        'rawUa': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.1 Safari/605.1.15',
+        'referer': 'https://google.com.au'}
 
-	payload = {'search': appForURL, 'platform': 'iPadSoftware'}
+    payload = {'search': appForURL, 'platform': 'iPadSoftware'}
 
-	r = requests.get('https://theappstore.org/search.php', params=payload, headers=h, proxies=systemProxy)
+    r = requests.get('https://theappstore.org/search.php', params=payload, headers=h, proxies=systemProxy)
 
-	soup = BeautifulSoup(r.text, 'html.parser')
+    soup = BeautifulSoup(r.text, 'html.parser')
 
-	for item in soup.find_all('div', class_='appmain'):
-		title = item.find('div', class_='apptitle').text.translate({ord(' '): None}).strip()
-		appURL = item.find('a', href=True)['href']
-		appID = find_ID(appURL)
+    for item in soup.find_all('div', class_='appmain'):
+        title = item.find('div', class_='apptitle').text.translate({ord(' '): None}).strip()
+        appURL = item.find('a', href=True)['href']
+        appID = find_id(appURL)
 
-		s = SequenceMatcher(a=title, b=appToFind)
-		
-		# Find anything that has a very high match ratio and create a dict of its values then
-		# append those to the matches list
-		if s.ratio() > .80:
-			match = {'title': title,
-					 'ID'	: appID,
-					 'ratio': s.ratio()}
-			matches.append(match)
-			count += 1
+        s = SequenceMatcher(a=title, b=appToFind)
 
-	if count == 0:
-		failures.append(app)
+        # Find anything that has a very high match ratio and create a dict of its values then
+        # append those to the matches list
+        if s.ratio() > .70:
+            match = {'title': title,
+                     'ID': appID,
+                     'ratio': s.ratio()}
+            matches.append(match)
+            count += 1
+
+    if count == 0:
+        failures.append(app)
 
 if matches:
-	print('We found %s out of %s matches' % (len(matches), len(apps)))
-	with open('results.csv', mode='w') as csv_file:
-		fieldnames = ['title', 'ID', 'bundleID', 'ratio']
-		writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-		
-		writer.writeheader() #write header outside of loop for only one entry
-		
-		# for each match, get the bundle ID out of json from request to itunes using the
-		# numeric app ID. Add bundle ID to dict and write all data to CSV
-		for m in matches:
-			itunesURL = 'https://itunes.apple.com/lookup?id=' + m['ID']
-			r = requests.get(itunesURL)
-			appManifest = r.json()['results']
-			m['bundleID'] = appManifest[0]['bundleId']
-			writer.writerow(m)
+    print('We found %s matches for %s apps' % (len(matches), len(apps)))
+    
+    if mode == 'csv':
+	    with open('results.csv', mode='w') as csv_file:
+	        fieldnames = ['title', 'ID', 'bundleID', 'ratio']
+	        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
 
-else: print('No matches found for any apps in list.')
+	        writer.writeheader()  # write header outside of loop for only one entry
+
+	        # for each match, get the bundle ID out of json from request to itunes using the
+	        # numeric app ID. Add bundle ID to dict and write all data to CSV
+	        for m in matches:
+	            itunesURL = 'https://itunes.apple.com/lookup?id=' + m['ID']
+	            r = requests.get(itunesURL)
+	            appManifest = r.json()['results']
+	            m['bundleID'] = appManifest[0]['bundleId']
+	            writer.writerow(m)
+    
+    if mode == 'single':
+
+    	for m in matches:
+	            itunesURL = 'https://itunes.apple.com/lookup?id=' + m['ID']
+	            r = requests.get(itunesURL)
+	            appManifest = r.json()['results']
+	            m['bundleID'] = appManifest[0]['bundleId']
+	            print('App name: {} - {}'.format(m['title'], m['bundleID']))
+
+
+else:
+    print('No matches found for any apps in list.')
 
 if failures:
-	for n in failures:
-		print('%s was not found in the store' % (n))
+    for n in failures:
+        print('%s was not found in the store' % (n))
